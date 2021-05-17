ee2d96a2be33bdb146f91e62055992b09e758ac0
casco55 <juanjo.desarrollador@gmail.com>
2021-05-17 15:45:13 -0400

    paginación

--- src/components/Guardados.js
+++ src/components/Guardados.js
@@ -1,8 +1,14 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const end_point = 'http://localhost/api_rest/api.php';
 
 class Guardados extends Component {
     state = {
         lista: [],
+        size: 10,
+        page: 1,
+        pages: 0,
     }
     /* actualizarLista = e =>{
         const data = JSON.parse(localStorage.getItem("lista"));
@@ -12,30 +18,130 @@ class Guardados extends Component {
         console.log(this.state.lista)
     }
     componentDidMount(){
-        const data = JSON.parse(localStorage.getItem("lista"));
+
+        axios.get(`${end_point}?page=${this.state.page}&size=${this.state.size}`)
+        .then(response => {
+            return response            
+        }).then(jsonResponse => {
+            const resultado = jsonResponse.data
+            console.log(resultado)
+            this.setState({
+                lista: resultado.urls,
+                pages: resultado.paginas
+            }, function(){
+                console.log(this.state.pages)
+            }) 
+        });     
+        /* const data = JSON.parse(localStorage.getItem("lista"));
         this.setState({lista: data})
-        console.log('renderizado')
+        console.log('renderizado') */
+    }
+    selectSizePage = e => {
+        this.setState({size: e.target.value}, function(){
+        axios.get(`${end_point}?page=${this.state.page}&size=${this.state.size}`)
+        .then(response => {
+            return response            
+        }).then(jsonResponse => {
+            const resultado = jsonResponse.data
+            console.log(resultado)
+            this.setState({
+                lista: resultado.urls,
+                pages: resultado.paginas
+            }, function(){
+                
+            }) 
+        });     
+        })
+    }
+    selectPage = e => {
+        this.setState({page: e.target.value}, function(){
+        axios.get(`${end_point}?page=${this.state.page}&size=${this.state.size}`)
+        .then(response => {
+            return response            
+        }).then(jsonResponse => {
+            const resultado = jsonResponse.data
+            console.log(resultado)
+            this.setState({
+                lista: resultado.urls,
+            }, function(){
+                console.log(this.state.pages)
+            }) 
+        });     
+        })
+    }
+    handleNextPage = e =>{
+        this.setState({page: this.state.page+1}, function(){
+            axios.get(`${end_point}?page=${this.state.page}&size=${this.state.size}`)
+            .then(response => {
+                return response            
+            }).then(jsonResponse => {
+                const resultado = jsonResponse.data
+                console.log(resultado)
+                this.setState({
+                    lista: resultado.urls,
+                }, function(){
+                    
+                }) 
+            });     
+            })
+    }
+    handlePrevPage = e =>{
+        this.setState({page: this.state.page-1}, function(){
+            axios.get(`${end_point}?page=${this.state.page}&size=${this.state.size}`)
+            .then(response => {
+                return response            
+            }).then(jsonResponse => {
+                const resultado = jsonResponse.data
+                console.log(resultado)
+                this.setState({
+                    lista: resultado.urls,
+                }, function(){
+                    
+                }) 
+            });     
+            })
     }
     
     render() { 
         
-        const {lista} = this.state;
+        const {lista,pages, page} = this.state;
+
+        const items = []
+        for (let i = 1; i <= pages; i++) {
+            items.push(<option value={i}>{i}</option>)
+            
+        }
 
         return (
-            <React.Fragment> 
+            <React.Fragment>
+
+                <div className="container my-5">
+                    <div className="row">
+                       <select onChange={this.selectSizePage}>
+                            <option value="10" selected>10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                       </select>
+                       <select onChange={this.selectPage}>
+                            {items}
+                       </select>
+                    </div>
+                </div>
                 
                 <div className="container my-5" id="imagenes-guardas">
                
                     <h2 className="text-center" onClick={this.mostrarLista}>Imágenes Guardadas</h2>
                     <div className="row">
-                    {lista.map((imagen) =>(
+                     {lista.map((imagen) =>(
                             
                             <div className="col-lg-4 my-3 d-flex flex-column">
-                                <img src={imagen} className="img-fluid"/>                   
+                                <img src={imagen.url} className="img-fluid"/>                   
                                
                             </div>
-                        ))}
+                        ))} 
                         
+                    {page > 1 && <button className="btn btn-info" onClick={this.handlePrevPage}>Anterior</button>}  
+                   {page >=1 && page != pages && <button className="btn btn-info" onClick={this.handleNextPage}>Siguiente</button>}
                     </div>
                 </div>
             </React.Fragment>
--- src/components/Imagenes.js
+++ src/components/Imagenes.js
@@ -20,50 +20,93 @@ class Imagenes extends Component {
     state = { 
         nombre: '',
         pagina: 0,
+        paginas: 0,
         img: [],
-        lista: []
+        url: '',
+        size: 10,
+        items:[],
    }
 
    nombreImagen = e => {
     this.setState({nombre: e.target.value })
     }
+
+    sizePage = e => {
+        this.setState({size: e.target.value}, function(){
+            axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}&size=${this.state.size}`)
+            .then(response => {
+                return response            
+            }).then(jsonResponse => {
+                console.log(jsonResponse)
+                const resultado = jsonResponse.data
+                this.setState({
+                    img: resultado.results,
+                    paginas: resultado.total_pages
+                }, function(){
+                    const llenado = [];        
+                    for (let i = 1; i <= this.state.paginas; i++) {
+                        const item = {'valor': i}
+                        llenado.push(item)            
+                    }
+                    console.log(llenado)
+                    this.setState({items: llenado}, function(){
+                    console.log(this.state.items)
+                    })
+                })
+            })
+        })
+    }
    
     
 
    handleSubmit = e => {
-    this.setState({pagina: 1}, function () {
-        const data = JSON.parse(localStorage.getItem("lista"));
-        this.setState({lista: data});
-        console.log(this.state.lista)
-        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}`)
+        if(this.state.nombre == ""){
+            alert('debe completar el campo de buscar');
+        }else{
+        this.setState({pagina: 1}, function () {
+        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}&size=${this.state.size}`)
         .then(response => {
             return response            
         }).then(jsonResponse => {
             console.log(jsonResponse)
-            const resultado = jsonResponse.data.results
+            const resultado = jsonResponse.data
             this.setState({
-                img: resultado
+                img: resultado.results,
+                paginas: resultado.total_pages
+            }, function(){
+                const llenado = [];        
+                for (let i = 1; i <= this.state.paginas; i++) {
+                    const item = {'valor': i}
+                    llenado.push(item)            
+                }
+                console.log(llenado)
+                this.setState({items: llenado}, function(){
+                console.log(this.state.items)
+                document.ready = document.getElementById("selector").value = this.state.pagina;
+                })
+
             })
         })
     });     
-        
+    }    
    }
 
   
    handleNextPage = e =>{
     
     this.setState({pagina: this.state.pagina+1}, function () {
-        const data = JSON.parse(localStorage.getItem("lista"));
-        this.setState({lista: data});
-        console.log(this.state.lista)
-        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}`)
+        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}&size=${this.state.size}`)
         .then(response => {
             return response            
         }).then(jsonResponse => {
             console.log(jsonResponse)
-            const resultado = jsonResponse.data.results
+            const resultado = jsonResponse.data
             this.setState({
-                img: resultado
+                img: resultado.results,
+                paginas: resultado.total_pages
+            }, function(){
+                console.log(this.state.paginas)
+                document.ready = document.getElementById("selector").value = this.state.pagina;
             })
         })
     });
@@ -73,10 +116,7 @@ class Imagenes extends Component {
     
     handlePrevPage  = e =>{
         this.setState({pagina: this.state.pagina-1}, function () {
-            const data = JSON.parse(localStorage.getItem("lista"));
-        this.setState({lista: data});
-        console.log(this.state.lista)
-        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}`)
+        axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}&size=${this.state.size}`)
         .then(response => {
             return response            
         }).then(jsonResponse => {
@@ -84,6 +124,9 @@ class Imagenes extends Component {
             const resultado = jsonResponse.data.results
             this.setState({
                 img: resultado
+            }, function(){
+                console.log(this.state.paginas)
+                document.ready = document.getElementById("selector").value = this.state.pagina;
             })
         })
         });
@@ -91,27 +134,77 @@ class Imagenes extends Component {
     
     saveImage = e =>{
         e.preventDefault();
-        const url = e.target.href;
-        const nuevaLista = this.state.lista;
-        nuevaLista.push(url)
-        this.setState({lista: nuevaLista})
-        localStorage.setItem("lista", JSON.stringify(this.state.lista));
-        const data = JSON.parse(localStorage.getItem("lista"));
-        console.log(data);
+        const url_img = e.target.href;
+        this.setState({url: url_img}, function () {
+            console.log(this.state.url)
+            const saveImage = {"url": this.state.url}
+            console.log(saveImage)
+            axios.post(`${end_point}`, saveImage)
+            .then(response => {
+                if(response.data == "ok"){
+                    alert("Imagen agregada a favoritos");
+                }else if(response.data == "existe"){
+                    alert("Está imagen ya se encuentra en favoritos");
+                }else{
+                    alert("Error al conectar con el servidor");
+                }         
+            })
+        });
+    }
+
+    changePage = e =>{
+        this.setState({pagina: e.target.value}, function () {
+            axios.get(`${end_point}?query=${this.state.nombre}&page=${this.state.pagina}&size=${this.state.size}`)
+            .then(response => {
+                return response            
+            }).then(jsonResponse => {
+                console.log(jsonResponse)
+                const resultado = jsonResponse.data
+                this.setState({
+                    img: resultado.results,
+                    paginas: resultado.total_pages
+                }, function(){
+                    console.log(this.state.pagina)
+                })
+            })
+            });
     }
 
     render() { 
-        const {img, pagina, nombre} = this.state;
+        const {img, pagina, paginas,items, nombre} = this.state;
+
+        
         return (
             
             <React.Fragment>
                 <div className="container my-5">
                     <div className="row">
                        <input type="text" onChange={this.nombreImagen} />
+                       <select onChange={this.sizePage}>
+                            <option value="10" selected>10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                       </select>
                        <button onClick={this.handleSubmit}>Buscar</button>
                     </div>
                 </div>
                 <div className="container">
+                    {pagina !=0 && 
+                        <select id="selector" onChange={this.changePage}>
+                            {items.map((opcion) =>{
+                            
+                            
+
+                            const a = <option value={opcion.valor}>{opcion.valor}</option>
+                            return a
+                            } 
+                                 
+                                                               
+                            
+                        )}
+                        </select>
+                    }
+                    {pagina > 0 && <h2 className="text-center">Página {pagina} de {paginas}</h2>}
                     <div className="row">
                        
                         {img.map((imagen) =>(
@@ -125,8 +218,8 @@ class Imagenes extends Component {
                         ))}
                     
                     
-                   {pagina >0 && <button className="btn btn-info" onClick={this.handlePrevPage}>Anterior</button>}  
-                   {pagina >=0 && <button className="btn btn-info" onClick={this.handleNextPage}>Siguiente</button>}   
+                   {pagina > 1 && <button className="btn btn-info" onClick={this.handlePrevPage}>Anterior</button>}  
+                   {pagina >=1 && pagina != paginas && <button className="btn btn-info" onClick={this.handleNextPage}>Siguiente</button>}   
                     
                     </div>
                 </div>
